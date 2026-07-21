75a4be4ce823a1687aab8b0f640b0b66b10c1860
he.zixu <he.zixu@kenqingkeji.com>
2026-07-21 20:27:22 +0800

    linux环境中文字符

--- 神经网络/sc/main/grad_down/main.c
+++ 神经网络/sc/main/grad_down/main.c
@@ -3,6 +3,10 @@
 
 #include "math_base.h"
 
+#define 维数 2
+#define 初始步长 10.0
+#define 循环次数 200
+
 double f(double* arr, unsigned int n, void* other)
 {
 	(void)n;
@@ -12,16 +16,31 @@ double f(double* arr, unsigned int n, void* other)
 
 int main()
 {
-	double 数值[2] = {200,200};
+	double 数值[维数] = { 200, 200 };
+	double 起点[维数] = { 数值[0], 数值[1] };
+	double 理论最优点[维数] = { 1.5, -1.0 };
+	double 初始函数值 = f(数值, 维数, NULL);
 	double* 结果 = 数值;
-	梯度下降求解_本地修改(
+	double 最终步长 = 梯度下降求解_本地修改(
 		结果,
-		2,
+		维数,
 		f,
 		NULL,
-		10,
-		200
+		初始步长,
+		循环次数
 	);
-	printf("(%g, %g)\n", 数值[0], 数值[1]);
+
+	double 最终函数值 = f(结果, 维数, NULL);
+	double 与最优点距离平方 = 获取两点距离平方(结果, 理论最优点, 维数);
+
+	puts("gradient descent test");
+	puts("---------------------");
+	puts("function: f(x, y) = (x - 1)(x - 2) + (y + 5)(y - 3)");
+	printf("start:    (% .10g, % .10g), f = %.17g\n", 起点[0], 起点[1], 初始函数值);
+	printf("config:   dimension = %u, initial_step = %.10g, iterations = %u\n", 维数, 初始步长, 循环次数);
+	printf("expected: (% .10g, % .10g), f = %.17g\n", 理论最优点[0], 理论最优点[1], f(理论最优点, 维数, NULL));
+	printf("result:   (% .10g, % .10g), f = %.17g\n", 结果[0], 结果[1], 最终函数值);
+	printf("error:    squared_distance_to_expected = %.17g\n", 与最优点距离平方);
+	printf("step:     final_step = %.17g\n", 最终步长);
 	return 0;
 }
